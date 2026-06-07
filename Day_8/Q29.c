fb0d5408fd5b6769026a942ebd95a073465c37db
dia0I <diyatyagi909@gmail.com>
2026-06-07 22:26:57 +0530

    Day_8 uploaded

---
+++ Day_8/Q29.c
@@ -0,0 +1,23 @@
+// half pyramid
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of rows in the half pyramid: ");
+    scanf("%d",&n);
+    for (int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            if (i+j>=n+1)
+            
+                printf("*");
+            else
+                printf(" ");
+            
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
