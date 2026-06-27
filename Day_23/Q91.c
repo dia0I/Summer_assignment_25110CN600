5eb3e27dc68f0be59118ed35cfcd7062372d2fca
dia0I <diyatyagi909@gmail.com>
2026-06-27 14:16:01 +0530

    Day_23 uploaded

--- Day_23/Q91.c
+++ Day_23/Q91.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main()
 {
     int flag = 0;
     char str1[100], str2[100];
+    // input first string
     printf("Enter first word: ");
     fgets(str1, sizeof(str1), stdin);
     int len1 = 0;
 
+    // count length1 without spaces or 'newline' & convert string to lowercase
     for (int i = 0; i < strlen(str1); i++)
     {
         if (str1[i] != ' ' && str1[i] != '\n')
+        {
             len1++;
+            str1[i] = tolower(str1[i]);
+        }
     }
 
+    // input second string
     printf("Enter second word: ");
     fgets(str2, sizeof(str2), stdin);
     int len2 = 0;
 
+    // count length2 without spaces or 'newline' & convert string to lowercase
     for (int i = 0; i < strlen(str2); i++)
     {
         if (str2[i] != ' ' && str2[i] != '\n')
+        {
             len2++;
+            str2[i] = tolower(str2[i]);
+        }
     }
 
     if (len1 != len2)
